0f8c70c561903bafbf03c0fb0ac4563f437d7147
simran-malik <45056197+simran-malik@users.noreply.github.com>
2018-11-15 13:15:51 +0530

    Add files via upload

---
+++ Register.java
@@ -0,0 +1,15 @@
+package Register;
+    
+import javax.swing.JTextArea;
+
+class Register {
+
+    
+    public static void main(String[] args) {
+       
+        //SignUp newForm = new SignUp();
+        SignIn newSignIn = new SignIn();
+        //Trying newForm = new Trying();
+    }
+    
+}
---
+++ UserInfo.java
@@ -0,0 +1,21 @@
+package Register;
+
+class UserInfo {
+
+	String name;
+	String dob;
+	String address;
+	String email;
+	String username;
+	String password;
+}
+
+/**<
+ * Stores the registeration Information of the User.
+ * @var name
+ * @var dob
+ * @var address
+ * @var email
+ * @var username
+ * @var password
+*/
